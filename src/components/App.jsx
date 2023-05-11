fe1df9bc3cefe898d834176d40c0bf93db0a25d9
SerhiiRud <carmagsales@gmail.com>
2023-05-11 11:13:02 +0300

    first step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
+import { Route, Routes, NavLink, Link } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Movies from 'pages/Movies/Movies';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
+    </div>
+  );
 };
